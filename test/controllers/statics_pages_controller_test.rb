d9aba365aba5051c5fa7270e862e8432ff8a5234
Hiroki Yui <xxx_yyy_www_1234@yahoo.co.jp>
2019-09-19 17:38:44 +0900

    Second Add

--- test/controllers/statics_pages_controller_test.rb
+++ test/controllers/statics_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticsPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get statics_pages_home_url
     assert_response :success
+    assert_select "title","Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get statics_pages_help_url
     assert_response :success
+    assert_select "title","Help | Ruby on Rails Tutorial Sample App"
   end
 
 end
