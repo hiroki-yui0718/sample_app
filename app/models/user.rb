f59a3586221a8f07f5e0a73efb7c5b46f65af5a6
Hiroki Yui <xxx_yyy_www_1234@yahoo.co.jp>
2019-10-03 17:01:38 +0900

    UpUp Smash

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_secure_password
+    validates :name,presence:true,length:{maximum:50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email,presence:true,length:{maximum:50},
+               format:{with:VALID_EMAIL_REGEX}
+    validates :password,presence:true,length:{maximum:8}
+end
